62040c9fd2ec0bc9689064c99ec4bfe4bb0b8e2f
Lam Nhan <nhan.lh@raksul.com>
2024-01-22 21:03:43 +0700

    Refactor local storage

--- localstorage/index.ts
+++ localstorage/index.ts
@@ -8,9 +8,9 @@ export * from './helpers/increment';
 export * from './helpers/iterate';
 export * from './helpers/iterate-keys';
 export * from './helpers/remove';
-export * from './helpers/clear';
 export * from './helpers/remove-bulk';
 export * from './helpers/remove-by-prefix';
 export * from './helpers/remove-by-suffix';
+export * from './helpers/clear';
 
 export * from './service';
